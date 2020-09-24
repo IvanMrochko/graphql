2ce5c72e668fd177128a85fb544cf90babc7ad10
IvanMrochko <67654830+IvanMrochcko@users.noreply.github.com>
2020-09-24 14:21:07 +0300

    lists

--- schema/schema.js
+++ schema/schema.js
@@ -1,12 +1,15 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList} = graphql;
 
 const movies = [
 	{ id: '1', name: 'Pulp Fiction', genre: 'Crime' , directorId: '1'},
 	{ id: '2', name: '1984', genre: 'Sci-Fi', directorId: '2'},
-	{ id: 3, name: 'V for vendetta', genre: 'Sci-Fi-Triller',directorId: '3' },
-	{ id: 4, name: 'Snatch', genre: 'Crime-Comedy',directorId: '4' },
+	{ id: '3', name: 'V for vendetta', genre: 'Sci-Fi-Triller',directorId: '3' },
+    { id: '4', name: 'Snatch', genre: 'Crime-Comedy',directorId: '4' },
+    { id: '5', name: 'Reservoir Dogs', genre: 'Crime', directorId: '1' },
+	{ id: '6', name: 'The Hateful Eight', genre: 'Crime', directorId: '1' },
+	{ id: '7', name: 'Inglourious Basterds', genre: 'Crime', directorId: '1' },
 ];
 const directors = [
 	{ id: '1', name: 'Quentin Tarantino', age: 55 },
@@ -36,6 +39,12 @@ const DirectorType = new GraphQLObjectType({
       id: { type: GraphQLID },
       name: { type: GraphQLString },
       age: { type: GraphQLInt },
+      movies: {
+          type: new GraphQLList(MovieType),
+          resolve(parent,args){
+              return movies.filter(movie => movie.directorId === parent.id);
+          }
+      }
     }),
   });
 
@@ -55,6 +64,18 @@ const Query = new GraphQLObjectType({
         resolve(parent, args) {
           return directors.find(director => director.id == args.id);
         },
+    },
+    movies: {
+        type: new GraphQLList(MovieType),
+        resolve(parent,args){
+            return movies;
+        }
+    },
+    directors: {
+        type: new GraphQLList(DirectorType),
+        resolve(parent,args){
+            return directors;
+        }
     },
   }
 });
