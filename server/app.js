df44f6abbafb8f33954c49bddb4278edc8a62ef3
IvanMrochko <67654830+IvanMrochcko@users.noreply.github.com>
2020-11-25 16:15:24 +0200

    connected to mongo bd

--- server/app.js
+++ server/app.js
@@ -1,15 +1,33 @@
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const schema = require('../schema/schema');
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("../schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 3005;
 
-app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql:true
-}));
+mongoose
+  .connect("mongodb+srv://Ivan:qazwsx12@cluster0.cnkhk.mongodb.net/graphgl", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((error) => console.log(error));
 
-app.listen(PORT, err => {
-    err ? console.log(err) : console.log('Server started!');
-})
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+const dbConnection = mongoose.connection;
+dbConnection.on("error", (err) => console.log(`connection error: ${err}`));
+dbConnection.once("open", () => console.log("Connected to DB"));
+
+app.listen(PORT, (err) => {
+  err ? console.log(err) : console.log("Server started!");
+});
