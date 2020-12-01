403225581f4dd3dc3d7399e2078754cf49bfc044
IvanMrochko <maxivan41@gmail.com>
2020-12-01 09:41:16 +0200

    deploy takeoff

---
+++ frontendapp/build/precache-manifest.e074fa26a67ae2261d93de5385177bd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8e57b57962512632c15",
+    "url": "/static/css/main.f2ecb6fd.chunk.css"
+  },
+  {
+    "revision": "b8e57b57962512632c15",
+    "url": "/static/js/main.b8e57b57.chunk.js"
+  },
+  {
+    "revision": "1d476c57edcefd8df9f6",
+    "url": "/static/js/1.1d476c57.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "229421f983b75fc33efe05e6c703a05d",
+    "url": "/index.html"
+  }
+];
