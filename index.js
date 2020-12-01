1517dfdb6b6aa78f8c90af9e7953447d7b7218c5
IvanMrochko <maxivan41@gmail.com>
2020-12-01 09:46:35 +0200

    final connect

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 const PORT = 5000;
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "frontendapp/build")));
 
 mongoose
   .connect("mongodb+srv://Ivan:qazwsx12@cluster0.cnkhk.mongodb.net/graphgl", {
@@ -30,6 +34,11 @@ app.use(
 const dbConnection = mongoose.connection;
 dbConnection.on("error", (err) => console.log(`connection error: ${err}`));
 dbConnection.once("open", () => console.log("Connected to DB"));
+
+// Anything that doesn't match the above, send back the index.html file
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/frontendapp/build/index.html"));
+});
 
 app.listen(PORT, (err) => {
   err ? console.log(err) : console.log("Server started!");
